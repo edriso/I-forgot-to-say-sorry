71c159d2d35853e1705c0214978e00beeb261f2a
Mohamed Idris <edriso.dev@gmail.com>
2025-08-24 17:36:04 +0300

    Implement image download functionality in script.js and adjust textarea resizing in style.css

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,14 @@ const generateBtn = document.querySelector('.generate-btn');
 generateBtn.addEventListener('click', async () => {
   generateBtn.style.display = 'none';
 
-  // TODO: Download the image
-  alert('Apology image generated and downloaded!');
+  const image = await htmlToImage.toPng(document.body);
+  const a = document.createElement('a');
+  a.href = image;
+  a.download = 'apology.png';
+  a.click();
+  a.remove();
 
-  generateBtn.style.display = 'block';
+  generateBtn.style.display = 'inline-block';
+
+  alert('Apology image generated and downloaded!');
 });
