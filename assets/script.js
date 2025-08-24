ad89f7f8cab66f3024a3517544084a45fcdcc670
Mohamed Idris <edriso.dev@gmail.com>
2025-08-24 17:31:58 +0300

    Remove package files and .gitignore; add script.js for image generation functionality in index.html

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+const generateBtn = document.querySelector('.generate-btn');
+
+generateBtn.addEventListener('click', async () => {
+  generateBtn.style.display = 'none';
+
+  // TODO: Download the image
+  alert('Apology image generated and downloaded!');
+
+  generateBtn.style.display = 'block';
+});
